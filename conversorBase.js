99ab5a845734749d6fdbea62dc3229510bbfa442
gabs-mvb <gabriel.boas@sptech.school>
2022-09-11 16:08:35 -0300

    Calculadora de Bases finalizada

---
+++ conversorBase.js
@@ -0,0 +1,25 @@
+function realizarCalculo() {
+  var valorDecimal = Number(numeroDecimal.value);
+
+  //logica de transformar o decimal em binario
+
+  let binario = valorDecimal.toString(2);
+
+  //Mostrar na tela o resultado do calculo para binario
+  resultadoBinario.innerHTML = binario;
+  console.log(binario);
+
+  //Logica de transformar o decimal em octal
+
+  let octal = valorDecimal.toString(8);
+  //Mostrar na tela o resultado do calculo para octal
+  resultadoOctal.innerHTML = octal;
+  console.log(binario);
+
+  //Logica de transformar o decimal em octal
+
+  let hexa = valorDecimal.toString(16);
+  //Mostrar na tela o resultado do calculo para octal
+  resultadoHexadecimal.innerHTML = hexa;
+  console.log(hexa);
+}
